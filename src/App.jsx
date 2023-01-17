bc34abdccd715d1daac63beb7063a91c7e7138c2
Jenny Moon <jenny0moon@gmail.com>
2023-01-17 16:31:20 +0000

    Add nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import {
+  Typography,
+  AppBar,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Toolbar,
+  Container,
+} from '@mui/material';
+import CameraIcon from '@mui/icons-material/PhotoCamera';
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <CameraIcon />
+          <Typography variant="h6">Photo Album</Typography>
+        </Toolbar>
+      </AppBar>
+    </>
   );
 };
 
